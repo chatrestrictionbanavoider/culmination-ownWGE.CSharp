2e07da066335d6d85f78cf093978a3f5c3a2c741
Michael T. Miyoshi <18077267+MichaelTMiyoshi@users.noreply.github.com>
2023-01-16 13:41:09 -0800

    01/16/2023 Making the ship move.

--- MyGame/Ship.cs
+++ MyGame/Ship.cs
@@ -7,6 +7,8 @@ namespace MyGame
 {
     class Ship : GameObject
     {
+        private const float Speed = 0.3f;
+
         private readonly Sprite _sprite = new Sprite();
         // Creates our ship.
         public Ship()
@@ -24,6 +26,16 @@ namespace MyGame
         public override void Update(Time elapsed)
         {
             Vector2f pos = _sprite.Position;
+            float x = pos.X;
+            float y = pos.Y;
+
+            int msElapsed = elapsed.AsMilliseconds();
+
+            if (Keyboard.IsKeyPressed(Keyboard.Key.Up))     { y -= Speed * msElapsed; }
+            if (Keyboard.IsKeyPressed(Keyboard.Key.Down))   { y += Speed * msElapsed; }
+            if (Keyboard.IsKeyPressed(Keyboard.Key.Left))   { x -= Speed * msElapsed; }
+            if (Keyboard.IsKeyPressed(Keyboard.Key.Right))  { x += Speed * msElapsed; }
+            _sprite.Position = new Vector2f(x, y);
         }
     }
 }
