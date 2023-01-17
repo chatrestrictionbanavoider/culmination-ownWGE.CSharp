f31f83f100cb2f4893fde6a965a00d953b245555
Michael T. Miyoshi <18077267+MichaelTMiyoshi@users.noreply.github.com>
2023-01-16 18:26:39 -0800

    01/16/2023 Animations.  Also included how to make the animation show up (modify Meteor.cs).

---
+++ MyGame/Explosion.cs
@@ -0,0 +1,44 @@
+﻿using GameEngine;
+using SFML.Graphics;
+using SFML.System;
+using System.Collections.Generic;
+
+namespace MyGame
+{
+    class Explosion : AnimatedSprite
+    {
+        public Explosion(Vector2f pos) : base(pos)
+        {
+            Texture = Game.GetTexture("Resources/explosion-spritesheet.png");
+            SetUpExplosionAnimation();
+            PlayAnimation("explosion", AnimationMode.OnceForwards);
+        }
+
+        public override void Update(Time elapsed)
+        {
+            base.Update(elapsed);
+
+            if (!IsPlaying())
+            {
+                MakeDead();
+            }
+        }
+
+        private void SetUpExplosionAnimation()
+        {
+            var frames = new List<IntRect>
+            {
+                new IntRect(  0, 0, 64, 64),    // frame 1
+                new IntRect( 64, 0, 64, 64),    // frame 2
+                new IntRect(128, 0, 64, 64),    // frame 3
+                new IntRect(192, 0, 64, 64),    // frame 4
+                new IntRect(256, 0, 64, 64),    // frame 5
+                new IntRect(320, 0, 64, 64),    // frame 6
+                new IntRect(384, 0, 64, 64),    // frame 7
+                new IntRect(448, 0, 64, 64),    // frame 8
+                new IntRect(512, 0, 64, 64)    // frame 9
+            };
+            AddAnimation("explosion", frames);
+        }
+    }
+}
--- MyGame/Meteor.cs
+++ MyGame/Meteor.cs
@@ -49,6 +49,11 @@ namespace MyGame
             {
                 otherGameObject.MakeDead();
             }
+            Vector2f pos = _sprite.Position;
+            pos.X = pos.X + (float)_sprite.GetGlobalBounds().Width / 2.0f;
+            pos.Y = pos.Y + (float)_sprite.GetGlobalBounds().Height / 2.0f;
+            Explosion explosion = new Explosion(pos);
+            Game.CurrentScene.AddGameObject(explosion);
             MakeDead();
         }
     }
